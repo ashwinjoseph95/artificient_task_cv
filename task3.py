056b2701cb40cb33941e780edc5c6a0d400d39f6
Ashwin Joseph Kurian <ashwin.joseph.kurian@rwth-aachen.de>
2023-04-28 23:35:36 +0200

    Updating task 3 code

--- task3.py
+++ task3.py
@@ -13,11 +13,54 @@ Note:
 import numpy as np
 import torch
 import torch.nn as nn
-
-
+import onnx
+import onnx2pytorch
+import torch.nn as nn
+from torchsummary import summary
 
 torch.manual_seed(8)    # DO NOT MODIFY!
 np.random.seed(8)   # DO NOT MODIFY!
 
 
 # write your code here ...
+# Load the ONNX model
+model = onnx.load("model/model.onnx")
+
+# Extract the input and output names
+input_name = model.graph.input[0].name
+output_name = model.graph.output[0].name
+
+# Define the PyTorch model
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        # Initialize the layers
+        self.conv_block1 = self.conv_block(3, 32)
+        self.sigmoid=nn.Sigmoid()
+        
+
+    def conv_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            
+        )
+        return block
+    
+    def block1(self,input):
+        x = self.conv_block1(input)
+        x1=self.sigmoid(x)
+        x_out = torch.multiply(x, x1)
+        return x_out
+
+    def forward(self, x):
+        # Pass the input through the layers
+        x = self.block1(x)
+
+        return x
+
+# Create an instance of the model and print a summary of the layers
+model = Model()
+summary(model, (3, 160, 320))
+torch.save(model, "trial.onnx")
